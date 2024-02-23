53e9ac581e347a517a09430664c8ea7516322f5f
peulsilva <peulsilva9@gmail.com>
2024-02-23 22:42:55 +0100

    add second solution to best time to buy and sell stock

--- problems/best-time-to-buy-and-sell-stock.py
+++ problems/best-time-to-buy-and-sell-stock.py
@@ -43,3 +43,41 @@ class Solution:
 
         return best, min(min_left, min_right), max(max_left, max_right)
         
+
+# Solution 2
+from collections import deque
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+
+        # max_right[i] := max_{j >= i } prices[j]
+        # min_left[i] := min_{j <= i} prices[j]
+
+        # max_profit = max_i {max_right[i] - min_left[i]}
+
+        max_right = deque()
+        min_left = []
+
+        m_left = 1e6
+        M_right = -1
+        
+        for i in range(n):
+            if prices[i] < m_left:
+                m_left = prices[i]
+
+            min_left.append(m_left)
+
+            if prices[n - i - 1] > M_right :
+                m_right = prices[n-i-1]
+
+            max_right.appendleft(m_right)
+            
+        max_profit = 0
+        
+        for i in range(n-1):
+            profit = max_right[i+1] - min_left[i]
+            if max_profit < profit:
+                max_profit = profit
+
+        return max_profit
+        
