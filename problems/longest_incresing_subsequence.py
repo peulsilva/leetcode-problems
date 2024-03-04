b63735e1f420ce9cf22df59e59f74fb7c3307fe6
peulsilva <peulsilva9@gmail.com>
2024-03-04 21:12:41 +0100

    add solution 2 to LIS

--- problems/longest_incresing_subsequence.py
+++ problems/longest_incresing_subsequence.py
@@ -15,3 +15,21 @@ class Solution:
                     LIS[l] = max(LIS[l], 1+ LIS[r])
 
         return max(LIS)
+    
+# solution 2
+    
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        # memo[i]: length of LIS from 0 to [i]
+        memo = [1 for i in nums]
+        n = len(nums)
+
+        for j in range(1,n):
+            # j > i
+            for i in range(j):
+
+                if nums[j] > nums[i]:
+                    memo[j] = max(memo[j], memo[i] + 1)
+
+        return max(memo) 
+
